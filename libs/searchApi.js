942a215e17cac314ce7f5c5cc05fb43c5c2644f8
AJAY <ajaynairarun@gmail.com>
2021-10-25 21:08:20 +0530

    add axios

--- libs/searchApi.js
+++ libs/searchApi.js
@@ -1,5 +1,5 @@
 import { createApi } from "unsplash-js";
-
+import axios from "axios";
 
 const getProfilesByName = async (name) => {
   const unsplash = await createApi({
@@ -7,9 +7,19 @@ const getProfilesByName = async (name) => {
   });
   let users = await unsplash.search.getUsers({
     query: name,
-    count: 100 ,
+    count: 100,
   });
-  return users
+  return users;
 };
-
-export default getProfilesByName;
+const getProfilesByAxios = async (name) => {
+  let url = `https://api.unsplash.com/search/users?page=1&query=${name}`;
+  let headers = {
+    headers: {
+      Authorization: "Client-ID " + process.env.NEXT_PUBLIC_API_KEY,
+    },
+  };
+  const users = await axios.get(url, headers);
+  console.log(users);
+  return { response: { results: users.data.results } };
+};
+export default getProfilesByAxios;
